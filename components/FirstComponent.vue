3b1dbd82e253bce4d0804fb3701c45604f853771
Akash Gangwar <akash.gangwar@exampathfinder.org>
2024-12-23 12:48:50 +0530

    components added

---
+++ components/FirstComponent.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>Hello, Vue!</h1>
+        <p>This is your first component.</p>
+    </div>
+</template>
+
+
+<style scoped>
+h1 {
+    color: #42b983;
+}
+</style>
